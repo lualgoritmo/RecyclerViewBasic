cca302fec9eb399087287b2a915ecc43a6dd3b97
lualgoritmo <lualgoritmo@outlook.com>
2023-07-01 16:37:19 -0300

    Subir layout

---
+++ RecyclerTest/app/src/main/java/com/example/recyclertest/ui/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.recyclertest.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import projeto.luciano.recyclertest.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
